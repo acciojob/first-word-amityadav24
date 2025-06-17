9272a32bd89cd16c4f02099fd76c4196823f21aa
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-18 00:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+  if (!s) return '';
+  s = s.trim();
+  const index = s.indexOf(' ');
+  return index === -1 ? s : s.slice(0, index);
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
